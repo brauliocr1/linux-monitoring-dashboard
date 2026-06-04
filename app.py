85882c62f763e4b5aaaaea3b881f806cf82a3dc7
Braulio Colin <brauliocolin1@DESKTOP-OUF9993.localdomain>
2026-06-04 12:23:20 -0600

    Prepare app for render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os 
 from flask import Flask
 import psutil
 import socket
@@ -121,4 +122,7 @@ def dashboard():
     return html
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
